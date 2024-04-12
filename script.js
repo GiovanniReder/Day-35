4026e1669ccd513eee9e656aae177a6bf304bfee
Giovanni <gio.reder@gmail.com>
2024-04-12 16:42:02 +0200

    modify delete method

--- script.js
+++ script.js
@@ -1,81 +1,98 @@
-document
-  .addEventListener("DOMContentLoaded", () => {
-    const addProduct = document.getElementById("form");
+document.addEventListener("DOMContentLoaded", () => {
+  const addProduct = document.getElementById("form");
 
-    addProduct.addEventListener("submit", (event) => {
-      event.preventDefault();
-      const name = document.getElementById("name").value;
-      const description = document.getElementById("description").value;
-      const brand = document.getElementById("brand").value;
-      const imageUrl = document.getElementById("imageUrl").value;
-      const price = document.getElementById("price").value;
+  addProduct.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const description = document.getElementById("description").value;
+    const brand = document.getElementById("brand").value;
+    const imageUrl = document.getElementById("imageUrl").value;
+    const price = document.getElementById("price").value;
 
-      const newProduct = {
-        name: name,
-        description: description,
-        brand: brand,
-        imageUrl: imageUrl,
-        price: price,
-      };
-      console.log(newProduct);
+    const newProduct = {
+      name: name,
+      description: description,
+      brand: brand,
+      imageUrl: imageUrl,
+      price: price,
+    };
+    console.log(newProduct);
 
-      /* METODO POST */
+    /* METODO POST */
 
-      fetch("https://striveschool-api.herokuapp.com/api/product/", {
-        method: "POST",
-        body: JSON.stringify(newProduct),
-        headers: {
-          "Content-Type": "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZTU2YzdmMzA0NjAwMWFlNTlmNWMiLCJpYXQiOjE3MTI5MDc2MjgsImV4cCI6MTcxNDExNzIyOH0.mbgB7Wr21qK9N-Li8pemb0rhJEewSIpjLYblJbWY5W0",
-        },
+    fetch("https://striveschool-api.herokuapp.com/api/product/", {
+      method: "POST",
+      body: JSON.stringify(newProduct),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZTU2YzdmMzA0NjAwMWFlNTlmNWMiLCJpYXQiOjE3MTI5MDc2MjgsImV4cCI6MTcxNDExNzIyOH0.mbgB7Wr21qK9N-Li8pemb0rhJEewSIpjLYblJbWY5W0",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("error");
+        }
       })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("error");
-          }
-        })
-        .then((data) => {
-          console.log(data);
-          event.target.reset();
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      /* METODO GET */
-
-      fetch("https://striveschool-api.herokuapp.com/api/product/", {
-        method: "GET",
-        body: JSON.stringify(newProduct),
-        headers: {
-          "Content-Type": "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZTU2YzdmMzA0NjAwMWFlNTlmNWMiLCJpYXQiOjE3MTI5MDc2MjgsImV4cCI6MTcxNDExNzIyOH0.mbgB7Wr21qK9N-Li8pemb0rhJEewSIpjLYblJbWY5W0",
-        },
+      .then((data) => {
+        console.log(data);
+        event.target.reset();
       })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("error");
-          }
-        })
-        .then((data) => {
-          console.log(data);
+      .catch((error) => {
+        console.log(error);
+      });
+
+    /* METODO GET */
+
+    fetch("https://striveschool-api.herokuapp.com/api/product/", {
+      method: "GET",
+
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZTU2YzdmMzA0NjAwMWFlNTlmNWMiLCJpYXQiOjE3MTI5MDc2MjgsImV4cCI6MTcxNDExNzIyOH0.mbgB7Wr21qK9N-Li8pemb0rhJEewSIpjLYblJbWY5W0",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("error while get");
+        }
+      })
+      .then((data) => {
+        const productCardContainer = document.getElementById(".container");
+        data.forEach((product) => {
+          const productCard = document.createElement("div");
+          productCard.classList.add("card", "my-3");
+          productCard.innerHTML = `
+            <div class="card-body">
+              <h5 class="card-title">${product.name}</h5>
+              <p class="card-text">${product.description}</p>
+              <p class="card-text">${product.brand}</p>
+              <p class="card-text">${product.price}</p>
+              <button id=deleteBtn_${product.id} class="btn btn-danger">Remove</button>
+              <button id=${product.id} class="btn btn-primary btn-edit">Edit</button>
+              `;
+          productCardContainer.appendChild(productCard);
+          const removeBtn = document.getElementById(`remove_${product.id}`);
+          removeBtn.addEventListener("click", function () {
+            removeProduct(product._id);
+          });
         });
-    });
-  })
-  .catch((error) => {
-    console.log(error);
+      })
+      .catch((error) => {
+        console.log("error while get", error);
+      });
   });
-
-const delProduct = document.getElementById("form");
+});
 
 /* METODO DELETE */
-delProduct.addEventListener("click", function () {
+
+const delBtn = document.getElementById("deleteBtn");
+delBtn.addEventListener("click", function () {
   const URL = "https://striveschool-api.herokuapp.com/api/product/";
   fetch(URL, {
     method: "DELETE",
@@ -86,13 +103,13 @@ delProduct.addEventListener("click", function () {
     },
   })
     .then((response) => {
-      if (!response.ok) {
+      if (response.ok) {
         throw new Error("Errore durante la richiesta DELETE");
       }
 
-      console.log("Prodotto cancellato");
+      console.log("Item deleted");
     })
     .catch((error) => {
-      console.error("Si è verificato un errore:", error);
+      console.log("Si è verificato un errore:", error);
     });
 });
